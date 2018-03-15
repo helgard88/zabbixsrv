d4dc82b3ffcc893e1cffef7fb08ee06e769ef6d1
helgard88 <merlin-88@ukr.net>
2018-03-16 01:06:46 +0200

    packages are ok

--- modules/zabbixsrv/manifests/init.pp
+++ modules/zabbixsrv/manifests/init.pp
@@ -12,10 +12,10 @@ class zabbixsrv (
     owner  => root,
     group  => root,
     mode   => '0644',
-    source => 'puppet:///environments/production/modules/zabbixsrv/etc/pki/rpm-gpg/RPM-GPG-KEY-ZABBIX',
+    source => 'puppet:///modules/zabbixsrv/etc/pki/rpm-gpg/RPM-GPG-KEY-ZABBIX',
   }
 
-  yumrepo { 'zabbixrepo':
+  yumrepo { 'zabbix':
     enabled  => 1,
     priority => 1,
     baseurl  => 'http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/',
@@ -26,14 +26,27 @@ class zabbixsrv (
     require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-ZABBIX'],
   }
 
+  # configure zabbix-nonsupported repo
+  yumrepo { 'zabbix-nonsupported':
+    enabled  => 1,
+    priority => 1,
+    baseurl  => 'https://repo.zabbix.com/non-supported/rhel/7/x86_64/',
+    gpgcheck => 0,
+    includepkgs => absent,
+    exclude     => absent,
+    gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-ZABBIX',
+    require     => Yumrepo['zabbix'],
+  }
+
+
   package { 'httpd':
     ensure => installed, 
   }
-
+  
   package { 'zabbix-server-mysql':
     ensure => installed,
     provider => 'yum',
-    require => yumrepo['zabbixrepo'],
+    require => Yumrepo['zabbix-nonsupported'],
   }
 
   package { 'zabbix-web-mysql':
